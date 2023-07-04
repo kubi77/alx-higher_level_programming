b8cebd3cfbdb2b18cd523099c8fa6040e0502121
kubi77 <ac512626@gmail.com>
2023-07-04 09:02:55 -0700

    Lazy matrix multiplication

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,13 @@
+/*
+ * File: python_string.h
+ */
+
+#ifndef PYTHON_STRING_H
+#define PYTHON_STRING_H
+
+#include "Python.h"
+
+void print_python_string(PyObject *p);
+
+#endif /* PYTHON_STRING_H */
+
