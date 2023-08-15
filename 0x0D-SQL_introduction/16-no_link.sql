edb9103bcae230edbb95c76711723dc23891d588
kubi77 <ac51262@gmail.com>
2023-08-15 09:45:59 -0700

    updated

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,11 @@
-SELECT score, name
-FROM second_table
-HAVING name IS NOT NULL
-ORDER BY score DESC;
+#!/bin/bash
+
+# Replace with your MySQL username and password
+MYSQL_USER="your_username"
+MYSQL_PASSWORD="your_password"
+
+# Replace with the actual database name
+DATABASE_NAME="hbtn_0c_0"
+
+# MySQL command to list records from the table second_table
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $DATABASE_NAME -e "SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;"
