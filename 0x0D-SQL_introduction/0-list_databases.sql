fa2d927f277a414cf6aa97ec05efa27420af3d83
kubi77 <ac51262@gmail.com>
2023-08-15 08:45:03 -0700

    updated

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,1 +1,8 @@
-SHOW DATABASES;
+#!/bin/bash
+
+# Replace with your MySQL username and password
+MYSQL_USER="kay"
+MYSQL_PASSWORD="100"
+
+# Run the MySQL command to list databases
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;"
